981371c0c35b263403d948917fda3b72bd5639aa
allahyarceferoff <callahyar@inbox.ru>
2024-04-03 18:09:45 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print(" Enter stars count:"); //3
+//        int count = scanner.nextInt();
+//Task 1
+//        for (int i = 0; i < count; i++) {
+//            for (int j = 0; j < count - i-1; j++) {        // i=0,  j=0,j=1  j<2    //i=1,  j=0,j<1   //i=2,  j<0
+//                System.out.print(" ");
+//            }
+//            for (int j = 0; j < 2 * i + 1; j++) {         //   i=0,   j=0                          j<1
+//                System.out.print("*");                    //   i=1,   j=0  j=1 j=2                 j<3
+//            }                                             //   i=2    j=0  j=1 j=2 j=3 j=4    j<5
+//            System.out.println();
+//        }
+
+
+//Task2
+//        for (int i = 0; i < count; i++) {
+//            for (int j = 0; j < count - i-1; j++) {       // i=0,  j=0,j=1  j<2    //i=1,  j=0,j<1   //i=2,  j<0
+//                System.out.print("  ");
+//            }
+//            for (int j = 0; j < 2 * i + 1; j++) {         //   i=0,   j=0                          j<1
+//                System.out.print("* ");                   //   i=1,   j=0  j=1 j=2                 j<3
+//            }                                             //   i=2    j=0  j=1 j=2 j=3 j=4    j<5
+//            System.out.println();
+//        }
+
+
+
+
+
+
+
+//Task 3
+//        for (int i = 0; i < count; i++) {
+//            for (int j = 0; j < count - i-1; j++) {
+//                System.out.print(" ");
+//            }
+//            for (int j = 0; j < 2 * i + 1; j++) {
+//                System.out.print(i+1);
+//            }
+//            System.out.println();
+//        }
+
+
+
+//Task 4
+
+
+                                                    //i<5
+                                                      // i=0,  j=0,j=1,j=2,j=3   j<4
+                                                   // i=1,  j=0,j=1,j=2       j<3
+                                                  // i=2,  j=0,j=1           j<2
+                                                 // i=3,  j=0               j<1
+
+
+
+
+
+                                                         //   i=1,   j=0  //j=1                j=2                 j<3
+                                                        //   i=2    j=0  //j=1 //j=2 //j=3     j=4                 j<5
+                                                       //   i=3    j=0                      j=6
+                                                        //   1  0 2    2 0 4   3 0 6  4 0 8   5  0 10
+
+
+
+
+
+
+
+        }
+
+    }
